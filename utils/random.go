937eb5c24f71c9d8c5aea4c03220fc462e42f042
Aimer Neige <aimer.neige.soft@gmail.com>
2022-02-27 16:19:29 +0000

    update utils

---
+++ utils/random.go
@@ -0,0 +1,13 @@
+package utils
+
+import "math/rand"
+
+// GetRandomShortLink get random short link
+func GetRandomShortLink(length int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
