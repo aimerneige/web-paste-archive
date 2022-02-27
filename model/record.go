2e0928a2dfc7d35ed72d5d34c5b98419b745875f
Aimer Neige <aimer.neige.soft@gmail.com>
2022-02-27 16:19:34 +0000

    update unique

--- model/record.go
+++ model/record.go
@@ -10,6 +10,6 @@ type Record struct {
 	ExpireTime int64  `gorm:"type:bigint"`
 	UserIP     string `gorm:"type:varchar(255)"`
 	UserAgent  string `gorm:"type:varchar(255)"`
-	ShortLink  string `gorm:"type:varchar(255)"`
-	RawLink    string `gorm:"type:varchar(255)"`
+	ShortLink  string `gorm:"type:varchar(255),unique"`
+	RawLink    string `gorm:"type:varchar(255),unique"`
 }
