f5d3e04fb7142a6d35bba99dd8a0778b703d32f2
Aimer Neige <aimer.neige.soft@gmail.com>
2022-02-27 15:50:01 +0000

    update blacklist middleware

--- route/all.go
+++ route/all.go
@@ -2,10 +2,12 @@ package route
 
 import (
 	"github.com/aimerneige/web-paste/controller"
+	"github.com/aimerneige/web-paste/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func AllRouteCollection(r *gin.Engine) *gin.Engine {
+	r.Use(middleware.BlackListMiddleware())
 	r.NoRoute(controller.NotFound)
 
 	return r
