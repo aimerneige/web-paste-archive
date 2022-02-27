5edf74090850e63f6d9c11eb91adb779d864924f
Aimer Neige <aimer.neige.soft@gmail.com>
2022-02-27 16:40:19 +0000

    update middleware

---
+++ middleware/record.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"github.com/aimerneige/web-paste/dto"
+	"github.com/aimerneige/web-paste/response"
+	"github.com/gin-gonic/gin"
+)
+
+// RecordCreateRequestCheck check request body
+func RecordCreateRequestCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var request dto.RecordRequestDto
+		err := c.ShouldBindJSON(&request)
+		if err != nil {
+			response.BadRequest(c, err, "Fail to bind request body.")
+			c.Abort()
+			return
+		}
+
+		if request.Content == "" {
+			response.BadRequest(c, nil, "Content is required.")
+			c.Abort()
+			return
+		}
+
+		if request.Title == "" {
+			request.Title = "Untitled"
+		}
+
+		c.Set("content", request.Content)
+		c.Set("title", request.Title)
+		c.Next()
+	}
+}
+
+// RecordPathIDCheck check path id
+func RecordPathIDCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id := c.Param("id")
+		if id == "" {
+			response.BadRequest(c, nil, "ID is required.")
+			c.Abort()
+			return
+		}
+
+		c.Set("id", id)
+		c.Next()
+	}
+}
