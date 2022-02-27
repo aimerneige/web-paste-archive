6a6ab176cd52c138a68893cff2a1aeb6534e643a
Aimer Neige <aimer.neige.soft@gmail.com>
2022-02-27 16:42:02 +0000

    update all

--- route/all.go
+++ route/all.go
@@ -4,12 +4,18 @@ import (
 	"github.com/aimerneige/web-paste/controller"
 	"github.com/aimerneige/web-paste/middleware"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func AllRouteCollection(r *gin.Engine) *gin.Engine {
 	r.Use(middleware.BlackListMiddleware())
+
 	record := r.Group("/record")
 	RecordRouteCollection(record)
+
+	staticPath := viper.GetString("common.path")
+	r.Static("/raw", staticPath)
+
 	r.NoRoute(controller.NotFound)
 
 	return r
